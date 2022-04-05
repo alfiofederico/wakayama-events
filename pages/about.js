9a21ba16d2ce23281533a349a2796fda39266674
Alfio Federico Di Pinto <fefferico81@gmail.com>
2022-04-06 07:19:27 +0900

    first commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p>Find the latest events in Wakayama!</p>
+      <p>Version 1.0.0</p>
+    </div>
+  )
+}
