106af8ed9addfb83d3f020d4bc15dc727b4b04f7
Alfio Federico Di Pinto <fefferico81@gmail.com>
2022-04-06 14:16:26 +0900

    Showcase added

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import styles from '../styles/Layout.module.css'
 import Header from './Header';
 import Footer from './Footer';
+import Showcase from './Showcase';
 
 export default function Layout({title, keywords, description, author, children}) {
+
+  const router = useRouter()
   return (
     <div>
       <Head>
@@ -14,6 +18,9 @@ export default function Layout({title, keywords, description, author, children})
       </Head>
 
       <Header />
+
+      { router.pathname === '/' && <Showcase />}
+
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome!</h1>
+      <h2>Find a Wakayama Event to Join!</h2>
+    </div>
+  )
+}
