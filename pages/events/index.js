dc76fada207ebd0151352bf49b9c9420a10577a9
Alfio Federico Di Pinto <fefferico81@gmail.com>
2022-04-13 22:13:18 +0900

    Pagination completed

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,28 +1,45 @@
 import Layout from "@/components/Layout";
-import { API_URL } from "@/config/index";
+import { API_URL, PER_PAGE } from "@/config/index";
 import EventItem from "@/components/EventItem";
 
-export default function EventsPage({ events }) {
+import Pagination from "@/components/Pagination";
+
+
+export default function EventsPage({ events, page, total }) {
+  const lastPage = Math.ceil(total / PER_PAGE);
+
   return (
     <Layout>
       <h1>Events</h1>
-      {events.length === 0 && <h3>No events for now...</h3>}
+      {events.length === 0 && <h3>No events to show</h3>}
 
       {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
+
+      <Pagination page={page} total={total}/>
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
-  const events = await res.json();
+export async function getServerSideProps({ query: { page = 1 } }) {
+  // Calculate Start Page
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+
+  // Fetch total/count
+  const totalRes = await fetch(`${API_URL}/events/count`);
+  console.log(totalRes);
+
+  const totalResHardCoded = 5;
+  const total = JSON.stringify(totalResHardCoded);
+
+  // Fetch Events
+  const eventRes = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  );
+  const events = await eventRes.json();
 
   return {
-    props: {
-      events,
-      revalidate: 1,
-    },
+    props: { events, page: +page, total },
   };
 }
