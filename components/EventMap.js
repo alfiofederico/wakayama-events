e2ac7e230db56c02614e3c1f8db57deddbc439d0
Alfio Federico Di Pinto <fefferico81@gmail.com>
2022-04-21 23:50:44 +0900

    addedgoogle map

--- components/EventMap.js
+++ components/EventMap.js
@@ -1,8 +1,9 @@
 import Image from "next/image"
 import {useState, useEffect} from 'react'
 import ReactMapGl, { Marker } from "react-map-gl";
+import Map from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-
+import Geocode from "react-geocode";
 
 
 
@@ -13,48 +14,50 @@ export default function EventMap({ evt }) {
   const [viewport, setViewport] = useState({
     latitude: 40.712772,
     longitude: -73.935242,
-    width: "100%",
-    height: "300px",
+    width: "100",
+    height: "500",
     mapStyle: "mapbox://styles/mapbox/streets-v11",
 
-    zoom: 9,
+    zoom: 12,
   });
  
-   useEffect(() => {
-     const requestOptions = {
-       method: "GET",
-     };
-     fetch(
-       `https://api.geoapify.com/v1/geocode/search?text=${evt.address}&apiKey=${process.env.NEXT_PUBLIC_GEOAPIFY_API_KEY}`,
-       requestOptions
-     )
-       .then((response) => response.json())
-       .then((result) => {
-         const lng = result.features[0].bbox[0];
-         const lat = result.features[0].bbox[1];
-         setLat(lat);
-         setLng(lng);
-         setViewport({ ...viewport, latitude: lat, longitude: lng });
-         setLoading(false);
-       })
-       .catch((error) => console.log("error", error));
-       console.log(lat, lng);
-   }, []);
+    useEffect(() => {
+      // Get latitude & longitude from address.
+      Geocode.fromAddress(evt.address).then(
+        (response) => {
+          const { lat, lng } = response.results[0].geometry.location;
+          setLat(lat);
+          setLng(lng);
+          setViewport({ ...viewport, latitude: lat, longitude: lng });
+          setLoading(false);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }, []);
+
+  Geocode.setApiKey(process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY);
 
    if (loading) return false;
 
    console.log(lat, lng)
    return (
-     <ReactMapGl
+     <Map
        {...viewport}
        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
-       onViewportChange={(vp) => setViewport(vp)}
-       style={{overflow:"visible", height:"500px", width:"100%",}}
+       onViewportChange={(vp) => setViewport(vp)} 
+       style={{
+         overflow: "visible",
+         height: "500px",
+         width: "100%",
+        
+       }}
      >
        <Marker key={evt.id} latitude={lat} longitude={lng}>
          <Image src="/images/pin.svg" width={30} height={30} />
        </Marker>
-     </ReactMapGl>
+     </Map>
    );
 }
 
