152b1e01d3c68db3d6257fd59ce9a87f03b86e6a
Alfio Federico Di Pinto <fefferico81@gmail.com>
2022-04-22 10:35:17 +0900

    Fixed the unresponsive map

--- components/EventMap.js
+++ components/EventMap.js
@@ -11,12 +11,12 @@ export default function EventMap({ evt }) {
   const [lat, setLat] = useState(null);
   const [lng, setLng] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [viewport, setViewport] = useState({
+  const [viewState, setViewState] = useState({
     latitude: 40.712772,
     longitude: -73.935242,
     width: "100",
     height: "500",
-    mapStyle: "mapbox://styles/mapbox/streets-v11",
+ 
 
     zoom: 12,
   });
@@ -28,7 +28,7 @@ export default function EventMap({ evt }) {
           const { lat, lng } = response.results[0].geometry.location;
           setLat(lat);
           setLng(lng);
-          setViewport({ ...viewport, latitude: lat, longitude: lng });
+          setViewState({ ...viewState, latitude: lat, longitude: lng });
           setLoading(false);
         },
         (error) => {
@@ -44,15 +44,15 @@ export default function EventMap({ evt }) {
    console.log(lat, lng)
    return (
      <Map
-       {...viewport}
+       {...viewState}
+       onMove={(evt) => setViewState(evt.viewState)}
        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
-       onViewportChange={(vp) => setViewport(vp)} 
        style={{
          overflow: "visible",
          height: "500px",
          width: "100%",
-        
        }}
+       mapStyle="mapbox://styles/mapbox/streets-v9"
      >
        <Marker key={evt.id} latitude={lat} longitude={lng}>
          <Image src="/images/pin.svg" width={30} height={30} />
