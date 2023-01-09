73674df374df32674fbaa7504b8f5eead8b21941
Alfio Federico Di Pinto <fefferico81@gmail.com>
2023-01-09 14:23:49 +0900

    logging the errors

--- pages/index.js
+++ pages/index.js
@@ -25,9 +25,11 @@ export default function HomePage({ events }) {
 export async function getStaticProps() {
   const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=3`);
   const events = await res.json();
-
+ console.log(res);
   return {
     props: { events },
     revalidate: 1,
   };
+
+  
 }
